3e375836a4f687fa1955a637a2256a806e272201
Farai Major <faraimutukumira@gmail.com>
2022-11-04 11:13:26 -0400

    compliment

--- compliment.py
+++ compliment.py
@@ -1,12 +1,15 @@
-# def reverse_complement(dna):
-#     complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
-#     return ''.join([complement[base] for base in dna[::-1]])
+def reverse_complement(dna):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    return ''.join([complement[base] for base in dna[::-1]])
 
 
-# s = "GTCAG"
-# print(reverse_complement(s))
+s = "GTCAG"
+print(reverse_complement(s))
 
 
 def similar(a):
     s = ["xyz", "foo", "of", 'aaa']
     print(similar(s))
+
+
+similar()
