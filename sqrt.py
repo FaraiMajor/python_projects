9f2e1f9c9e3c100d6fc69219f0a0b385f91728aa
Farai Major <faraimutukumira@gmail.com>
2022-11-17 20:49:47 -0500

    square root leetcode

---
+++ sqrt.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        bigger, now, smaller = x, max(x // 2, 1), 0
+        while True:
+            if now * now == x or smaller == now or bigger == now:
+                return now
+            else:
+                if (now)**2 < x:
+                    smaller, now = now, (now + bigger)//2
+                else:
+                    bigger, now = now, (now + smaller)//2
