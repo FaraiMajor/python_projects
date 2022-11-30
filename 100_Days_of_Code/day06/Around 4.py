45e3011c548024e21b2fb445133f62a18a0414aa
Farai Major <faraimutukumira@gmail.com>
2022-11-30 14:10:53 -0500

    reegorg game

---
+++ 100_Days_of_Code/day06/Around 4.py
@@ -0,0 +1,32 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def starter():
+    put()
+    turn_left()
+    turn_left()
+    move()
+    turn_right()
+
+
+starter()
+while True:
+    if front_is_clear():
+        move()
+    if right_is_clear():
+        turn_right()
+    if wall_in_front():
+        turn_left()
+    if object_here():
+        done()
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+# Link to game is below
+# http://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Around%201%20-%20apple&url=worlds%2Ftutorial_en%2Faround1c.json
+################################################################
