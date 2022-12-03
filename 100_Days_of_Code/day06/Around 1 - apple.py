3910633a9bc373923fbc027ca90587b68a9b8c7e
Farai Major <faraimutukumira@gmail.com>
2022-12-03 02:28:53 -0500

    reeborg game

--- 100_Days_of_Code/day06/Around 1 - apple.py
+++ 100_Days_of_Code/day06/Around 1 - apple.py
@@ -21,3 +21,5 @@ while True:
 # Link to game is below
 # http://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Around%201%20-%20apple&url=worlds%2Ftutorial_en%2Faround1c.json
 ################################################################
+
+# farai mutukumira
