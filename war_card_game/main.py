25de64caa8b48765b1d18ffc852782abc7e3bed4
Farai Major <faraimutukumira@gmail.com>
2022-12-08 21:38:03 -0500

    war card game

--- war_card_game/main.py
+++ war_card_game/main.py
@@ -1,4 +1,5 @@
 import random
+import conditions as art
 
 # card options
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
@@ -6,6 +7,19 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
          'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
           'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
+
+
+def replay():
+
+    res = input(
+        "Do you want to play again? Enter 'Y' for Yes or 'N' for No: ").lower()
+
+    if res == 'y':
+        game_play()
+    else:
+        print('GAME OVER, Hope you had fun')
+        print(art.over)
+# ******************************************************************************************
 
 #  card classs creates a card
 
@@ -20,6 +34,8 @@ class Card:
 
     def __str__(self):
         return self.rank + ' of ' + self.suit
+
+# ******************************************************************************************
 
 # Create a deck of 52 cards and enable shuffling and dealing
 
@@ -41,6 +57,8 @@ class Deck:
     def deal_one(self):
         # Pop to remove one card from the list of all_cards
         return self.all_cards.pop()
+
+# ******************************************************************************************
 
 
 class Player:
@@ -66,17 +84,104 @@ class Player:
     def __str__(self):
         return (f'Player {self.name} has {len(self.all_cards)}.')
 
-
 # ******************************************************************************************
-# Test cases
-deck = Deck()
-deck.shuffle()
-new = Player("Jose")
-for dek in deck.all_cards:
-    new.all_cards.append(dek)
-print(new)
-for dick in new.all_cards:
-    print(dick)
-remove = new.remove_one()
-print(f'Card \"{remove}\" was removed')
-print(new)
+
+
+print(art.logo)
+print(art.card)
+
+
+def game_play():
+
+    player_one = Player("one")
+    player_two = Player("two")
+
+    new_deck = Deck()
+    new_deck.shuffle()
+
+    for x in range(26):
+        player_one.add_cards(new_deck.deal_one())
+        player_two.add_cards(new_deck.deal_one())
+
+    game_on = True
+    round_num = 0
+
+    while game_on:
+
+        round_num += 1
+        # print(f"Round {round_num}")
+
+        if len(player_one.all_cards) == 0:
+            print("Player One, out of cards!")
+            print(art.two)
+            game_on = False
+            break
+
+        if len(player_two.all_cards) == 0:
+            print("Player Two, out of cards!")
+            print(art.one)
+            game_on = False
+            break
+
+        # STAER A NEW ROUND
+        # Otherwise, the game is still on!
+
+        # Start a new round and reset current cards "on the table"
+        player_one_cards = []
+        player_one_cards.append(player_one.remove_one())
+
+        player_two_cards = []
+        player_two_cards.append(player_two.remove_one())
+
+        at_war = True
+
+        while at_war:
+
+            if player_one_cards[-1].value > player_two_cards[-1].value:
+
+                # Player One gets the cards
+                player_one.add_cards(player_one_cards)
+                player_one.add_cards(player_two_cards)
+
+                # No Longer at "war" , time for next round
+                at_war = False
+
+            # Player Two Has higher Card
+            elif player_one_cards[-1].value < player_two_cards[-1].value:
+
+                # Player Two gets the cards
+                player_two.add_cards(player_one_cards)
+                player_two.add_cards(player_two_cards)
+
+                # No Longer at "war" , time for next round
+                at_war = False
+
+            else:
+                # print('WAR!')
+                # This occurs when the cards are equal.
+                # We'll grab another card each and continue the current war.
+
+                # First check to see if player has enough cards
+
+                # Check to see if a player is out of cards:
+                if len(player_one.all_cards) < 3:
+                    print("Player One unable to play war! Game Over at War")
+                    print(art.two)
+                    game_on = False
+                    break
+
+                elif len(player_two.all_cards) < 3:
+                    print("Player Two unable to play war! Game Over at War")
+                    print(art.one)
+                    game_on = False
+                    break
+                # Otherwise, we're still at war, so we'll add the next cards
+                else:
+                    for _ in range(3):
+                        player_one_cards.append(player_one.remove_one())
+                        player_two_cards.append(player_two.remove_one())
+    replay()
+
+
+game_play()
+# ******************************************************************************************
