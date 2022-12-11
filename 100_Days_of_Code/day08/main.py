4f9ac7d61e4498c1467b9717844115ee218c4de3
Farai Major <faraimutukumira@gmail.com>
2022-12-10 20:46:30 -0500

    caesar cipher

--- 100_Days_of_Code/day08/main.py
+++ 100_Days_of_Code/day08/main.py
@@ -1,6 +1,9 @@
 # Caesar Cipher
 import string
+import arts as art
 
+
+print(art.logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -9,26 +12,41 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(text, shift):
-    word = ''
-    for i in text:
-        # index = alphabet.index(i) + shift
-        index = ord(i) - ord('a') + shift
-        word += (string.ascii_letters[index % len(string.ascii_letters)])
-    print(word)
+def replay():
+    res = input(
+        "Do you want to play again? Enter 'Y' for Yes or 'N' for No: ").lower()
+
+    if res == 'y':
+        direction = input(
+            "Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        caesar(text, shift, direction)
+    else:
+        print('GAME OVER, Hope you had fun')
+        print(art.over)
 
 
-def decrpyt(text, shift):
-    word = ''
-    for i in text:
-        index = ord(i) - ord('a') - shift
-        word += (string.ascii_letters[index % len(string.ascii_letters)])
-    print(word)
+def caesar(text, shift, direction):
+
+    final_word = ''
+    if direction == 'encode':
+        for char in text:
+            # index = alphabet.index(char) + shift
+            index = ord(char) - ord('a') + shift
+            final_word += (string.ascii_letters[index %
+                           len(string.ascii_letters)])
+
+    elif direction == 'decode':
+        for char in text:
+            index = ord(char) - ord('a') - shift
+            final_word += (string.ascii_letters[index %
+                           len(string.ascii_letters)])
+    else:
+        pass
+
+    print(final_word)
+    replay()
 
 
-if direction == 'encode':
-    encrypt(text, shift)
-elif direction == 'decode':
-    decrpyt(text, shift)
-else:
-    pass
+caesar(text, shift, direction)
