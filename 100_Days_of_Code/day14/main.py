f25dbf6d1764e7c184ca44992a0166d3bc65b7dc
Farai Major <faraimutukumira@gmail.com>
2022-12-22 23:53:43 -0500

    higher_lower_final

--- 100_Days_of_Code/day14/main.py
+++ 100_Days_of_Code/day14/main.py
@@ -1,11 +1,12 @@
 # higher lower sart
-import arts as art
+from arts import vs, over, logo
 from game_data import data
 import random
 import os
 
 
 def random_selection():
+    """Get data from random account"""
     return random.choice(data)
 
 
@@ -18,7 +19,7 @@ def format_data(selection):
 
 
 def check_guess(guess, a_followers, b_followers):
-    """Checks followers against user's guess 
+    """Checks followers against user's guess
   and returns True if they got it right.
   Or False if they got it wrong."""
     if a_followers > b_followers:
@@ -27,13 +28,21 @@ def check_guess(guess, a_followers, b_followers):
         return guess == 'b'
 
 
-def game():
-    print(art.logo)
-    score = 0
+def replay():
+    answer = input(
+        'Do you want to play again? Enter Y for Yes or N for no: ').lower()
+    if answer == 'y':
+        game()
+    else:
+        print(over)
 
+
+def game():
+    print(logo)
+    score = 0
+    game_on = True
     selection_a = random_selection()
     selection_b = random_selection()
-    game_on = True
 
     while game_on:
         selection_a = selection_b
@@ -43,26 +52,57 @@ def game():
             selection_b = random_selection()
 
         print(f"Compare A: {format_data(selection_a)}.")
-        print(f'{art.vs}'.center(40))
+        print(f'{vs}'.center(40))
         print(f"Against B: {format_data(selection_b)}.")
 
         guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
         # get follower_count and call check guess to compare
 
-        selection_a_count = selection_a['follower_count']
-        selection_b_count = selection_b['follower_count']
-        is_correct = check_guess(guess, selection_a_count, selection_b_count)
+        a_follower_count = selection_a['follower_count']
+        b_follower_count = selection_b['follower_count']
+        is_correct = check_guess(guess, a_follower_count, b_follower_count)
 
         os.system('clear')
-        print(art.logo)
+        print(logo)
         if is_correct:
             score += 1
             print(f'Correct current score: {score}')
         else:
             game_on = False
             print(f'Sorry you got the wrong guess. Final score: {score}')
-            print(art.over)
+    replay()
 
 
 game()
+
+'''
+
+FAQ: Why does choice B always become choice A in every round, even when A had more followers? 
+
+Suppose you just started the game and you are comparing the followers of A - Instagram (364k) to B - Selena Gomez (174k). Instagram has more followers, so choice A is correct. However, the subsequent comparison should be between Selena Gomez (the new A) and someone else. The reason is that everything in our list has fewer followers than Instagram. If we were to keep Instagram as part of the comparison (as choice A) then Instagram would stay there for the rest of the game. This would be quite boring. By swapping choice B for A each round, we avoid a situation where the number of followers of choice A keeps going up over the course of the game. Hope that makes sense :-)
+
+'''
+
+
+# Generate a random account from the game data.
+
+# Format account data into printable format.
+
+# Ask user for a guess.
+
+# Check if user is correct.
+# Get follower count.
+# If Statement
+
+# Feedback.
+
+# Score Keeping.
+
+# Make game repeatable.
+
+# Make B become the next A.
+
+# Add art.
+
+# Clear screen between rounds.
