984ef750cb410987375fd19b2d73577263ba9221
Farai Major <faraimutukumira@gmail.com>
2022-12-23 21:10:18 -0500

    coffe machine

--- 100_Days_of_Code/day15/main.py
+++ 100_Days_of_Code/day15/main.py
@@ -1,1 +1,51 @@
 from art import logo
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+print(MENU["cappuccino"]['cost'])
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# TODO: 1. check this
+
+
+def resource_sufficient(customer_order):
+    for item in customer_order:
+        if customer_order[item] > resources[item]:
+            print(f"Sorry there is not enough {customer_order[item]}")
+
+
+def process_coins():
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
