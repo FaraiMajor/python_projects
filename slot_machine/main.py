7fc02f9a2801dacbccd8dfcb02931bb23835b1fb
Farai Major <faraimutukumira@gmail.com>
2023-01-04 13:43:30 -0500

    slot machine inouts dones

--- slot_machine/main.py
+++ slot_machine/main.py
@@ -1,8 +1,55 @@
-
+import random
 
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+# we have 4 symbols in the slot and here we assign quantity for each symbol so A is most valuable
+symbol_count = {
+    'A': 2,
+    'B': 4,
+    'C': 6,
+    'D': 8
+}
+
+
+def get_slot_spin(rows, cols, symbols):
+    all_symbols = []
+    # nested loop to add symbols the same amount as given in the dictionary
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    # insert into column not rows hence why we did not use [[1,2,3]] etc
+    """
+    1. create columns list to store a lists of single column
+    -goal here is to have symbols on colums like a slot so we will have 3 column in this manner
+    -columns = [[],[],[]]
+    2.loop with a range of columns we assigned and create a column list to store the symbol for eachb column
+    -while here make a copy of the sysmbols list to ensure that we loop through the symbols 3 times for 3 column
+    -How it works is we loop again (nested) for rows(reels) and we append a random sysmbol to our column list
+    -We then delete that symbol so that we dont select it again or no more than the symbols available
+    -Now this is where the cooy comes into play
+    -Copy list is what we will use to get a random value, delete it and not change out main symbols list
+    -This ensure that we can do the other 2 columns
+    3 after this we then ass that colomn list to our columns list
+    - in the end we then have a list of lists like this columns = [[],[],[]]
+    """
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
 
 
 def deposit():
@@ -39,10 +86,12 @@ def get_lines():
 
     return lines
 
+#  player places their bet here
+
 
 def get_bet():
     while True:
-        amount = input("How much would you like to bet? $")
+        amount = input("How much would you like to bet on each line? $")
         # authenticate if the input is a digit
         if amount.isdigit():
             amount = int(amount)
@@ -59,7 +108,17 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_lines()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(
+                f"Insufficient funds in your account!!. Your current balance is ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines}. Total bet is: ${total_bet}")
 
 
 main()
